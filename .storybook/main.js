7d79bc0a6cf243f3222a2ff806d61fb2eda34693
Chase Odermott <codermott@basspro.com>
2026-08-17 14:50:22 -0500

    adding html extractor

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,12 +4,16 @@ const config = {
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+
   addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
+    "@whitespace/storybook-addon-html",
   ],
+
   framework: "@storybook/html-vite",
 };
+
 module.exports = config;
